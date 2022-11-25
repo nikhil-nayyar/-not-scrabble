43078ee6fb8a985ebdf37a5bfe1ada639cae677a
nikhil-nayyar <nikhil.sanjiv.nayyar@gmail.com>
2022-11-25 10:24:16 -0500

    implement TileRack

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ mod board;
 mod constants;
 mod tiles;
 
-use crate::tiles::Tile;
 use crate::tiles::TileBag;
+use crate::tiles::TileRack;
 
 fn main() {
 
-    let sample_bag:TileBag = TileBag::new();
-    println!("{}", sample_bag);
+    let mut sample_bag:TileBag = TileBag::new();
+
+    let rack1 = TileRack::new(&mut sample_bag);
+    let rack2 = TileRack::new(&mut sample_bag);
+
+    println!("{}", rack1);
+    println!("{}", rack2);
     
     ();
 }
--- src/tiles.rs
+++ src/tiles.rs
@@ -3,6 +3,9 @@
 use core::fmt;
 use std::{char::from_u32};
 use std::collections::HashMap;
+use std::thread::sleep;
+use std::time;
+use rand::Rng;
 use crate::constants::LETTER_COUNT;
 use crate::constants::LETTER_VALUE;
 
@@ -15,15 +18,16 @@ pub struct Tile{
 impl Tile{
 
     pub fn new(letter: char, point_value: u8) -> Self{
-        
-
         let t:Self = Tile{
             letter: letter,
             point_value: point_value,
             point_subscript: Self::generate_subscript( point_value),
         };
         t
-
+    }
+    
+    pub fn get_letter(&self)->char{
+        self.letter
     }
 
     const UNICODE_SUBSCRIPT_BASE: u32 = 0x2080;
@@ -43,6 +47,7 @@ impl Tile{
 
     }
 
+
 }
 
 impl std::fmt::Display for Tile{
@@ -56,15 +61,18 @@ impl std::fmt::Display for Tile{
 
 pub struct TileBag{
     data: HashMap<char,Vec<Tile>>,
+    num: u8,
 }
 
 impl TileBag{
     pub fn new() -> Self{
 
         let mut generated: HashMap<char,Vec<Tile>> = HashMap::with_capacity(26);
+        let mut num: u8 = 0;
 
         for (letter, count) in LETTER_COUNT.iter(){
             let mut letter_vector: Vec<Tile> = Vec::new();
+            num+=count;
             for _ in 0..*count{
                 letter_vector.push(
                     Tile::new(*letter, LETTER_VALUE[letter])
@@ -73,36 +81,75 @@ impl TileBag{
             generated.insert( *letter, letter_vector);
         }
 
-        return TileBag { data: generated }
+        println!("Generated TileBag with {} tiles", num);
+
+        return TileBag { data: generated, num: num}
 
     }
-}
 
-/*
+    /*
+    pub fn draw_tile(&mut self) -> Option<Tile>{
 
-impl std::fmt::Display for TileBag{
+        println!("starting draw_tile()");
 
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
+        let mut rng = rand::thread_rng();
+        let mut result: Option<Tile> = None;
 
-        let mut result = String::new();
-
-        for (letter,tile_vector) in &self.data{
-            result.push(*letter);
-            result.push_str(" : ");
-            
-            for tile in tile_vector.iter(){
-                result = result + &tile.to_string();
+        if self.num == 0 {
+            println!("No tiles remaining\n");
+            result = None;
+        } 
+        else{
+            let mut found = false;
+            println!("loop");
+            while !found{
+                let letter_num: u8 = rng.gen_range(0..26);
+                let letter_char: char = (65+letter_num) as char;
+                println!("drawing from {}", letter_char);
+                if self.data[&letter_char].len() != 0 {
+                    found = true;
+                    result = self.data.get_mut(&letter_char).unwrap().pop()
+                }
             }
-
-            result.push('\n')
-
         }
 
-       write!(f,"{}", result)
+        result
+        
+    }
+    */
+
+    pub fn draw_tile(&mut self) -> Option<Tile>{
+
+        let mut rng = rand::thread_rng();
+        let mut result: Option<Tile> = None;
+
+        if self.num == 0 {
+            println!("No tiles remaining\n");
+            result = None;
+        } 
+        else{
+            let mut found = false;
+            while !found{
+                let letter_num: u8 = rng.gen_range(0..26);
+                let letter_char: char = (65+letter_num) as char;
+                if self.data[&letter_char].len() != 0 {
+                    found = true;
+                    result = self.data.get_mut(&letter_char).unwrap().pop();
+                    self.num -= 1;
+                }
+                sleep(std::time::Duration::from_millis(10));
+            }
+        }
+
+        result
+        
+    }
+
+    pub fn is_empty(&self) -> bool{
+        self.num == 0
     }
 
 }
-*/
 
 impl std::fmt::Display for TileBag{
 
@@ -126,6 +173,66 @@ impl std::fmt::Display for TileBag{
         }
 
        write!(f,"{}", result)
+    }
+}
+
+
+pub struct TileRack{
+    data: HashMap<char,Vec<Tile>>,
+    num: u8,
+}
+
+impl std::fmt::Display for TileRack{
+
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
+        let mut result = String::new();
+
+        for (_, vector) in &self.data {
+            for tile in vector {
+                result += &tile.to_string();
+            }
+        }
+        write!(f,"{}", result)
+    }
+
+}
+
+impl TileRack{
+
+    pub fn new(bag: &mut TileBag) -> Self{
+
+        let mut rack = TileRack{
+            data: HashMap::new(),
+            num: 0
+        };
+
+        rack.get_letters(bag);
+
+        rack
+
+    }
+
+    fn get_letters(&mut self, bag: &mut TileBag) -> (){
+
+        while self.num < 7 {
+            let t = bag.draw_tile().unwrap();
+            let t_char = &t.get_letter();
+
+            if self.data.contains_key(t_char) {
+                let vector = self.data.get_mut(&t.get_letter()).unwrap();
+                vector.push(t);
+            } else{
+                let mut vector = vec![];
+                vector.push(t);
+                self.data.insert(*t_char, vector);
+            }
+            self.num+=1;
+        }
+
+    }
+
+    pub fn get_count(&self) -> u8{
+        self.num
     }
 
 }
